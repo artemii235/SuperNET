d0df7377745991414b5975c3b9b06c92b5af7da7
Sergey O. Boyko <sergey.boyko0791@gmail.com>
2020-06-23 15:42:00 +0700

    Change detection of mine orders to ask.pubsecp == our_pubkey

--- mm2src/lp_ordermatch.rs
+++ mm2src/lp_ordermatch.rs
@@ -1523,13 +1523,11 @@ pub async fn orderbook(ctx: MmArc, req: Json) -> Result<Response<Vec<u8>>, Strin
     let base_coin: MmCoinEnum = try_s!(base_coin.ok_or("Base coin is not found or inactive"));
     let ordermatch_ctx: Arc<OrdermatchContext> = try_s!(OrdermatchContext::from_ctx(&ctx));
     let orderbook = try_s!(ordermatch_ctx.orderbook.lock());
-    let my_maker_orders = try_s!(ordermatch_ctx.my_maker_orders.lock());
-    let my_taker_orders = try_s!(ordermatch_ctx.my_taker_orders.lock());
+    let my_pubsecp = hex::encode(&**ctx.secp256k1_key_pair().public());
     let asks = match orderbook.get(&(req.base.clone(), req.rel.clone())) {
         Some(asks) => {
             let mut orderbook_entries = vec![];
             for (uuid, ask) in asks.iter() {
-                let is_mine = my_maker_orders.get(uuid).is_some() || my_taker_orders.get(uuid).is_some();
                 orderbook_entries.push(OrderbookEntry {
                     coin: req.base.clone(),
                     address: try_s!(base_coin.address_from_pubkey_str(&ask.pubsecp)),
@@ -1543,7 +1541,7 @@ pub async fn orderbook(ctx: MmArc, req: Json) -> Result<Response<Vec<u8>>, Strin
                     age: (now_ms() as i64 / 1000) - ask.timestamp as i64,
                     zcredits: 0,
                     uuid: *uuid,
-                    is_mine,
+                    is_mine: my_pubsecp == ask.pubsecp,
                 })
             }
             orderbook_entries
@@ -1555,7 +1553,6 @@ pub async fn orderbook(ctx: MmArc, req: Json) -> Result<Response<Vec<u8>>, Strin
             let mut orderbook_entries = vec![];
             for (uuid, ask) in asks.iter() {
                 let price_mm = MmNumber::from(1i32) / ask.price_rat.as_ref().map(|p| p.clone()).unwrap_or(from_dec_to_ratio(ask.price.clone()).into());
-                let is_mine = my_maker_orders.get(uuid).is_some() || my_taker_orders.get(uuid).is_some();
                 orderbook_entries.push(OrderbookEntry {
                     coin: req.rel.clone(),
                     address: try_s!(rel_coin.address_from_pubkey_str(&ask.pubsecp)),
@@ -1571,7 +1568,7 @@ pub async fn orderbook(ctx: MmArc, req: Json) -> Result<Response<Vec<u8>>, Strin
                     age: (now_ms() as i64 / 1000) - ask.timestamp as i64,
                     zcredits: 0,
                     uuid: *uuid,
-                    is_mine,
+                    is_mine: my_pubsecp == ask.pubsecp,
                 })
             }
             orderbook_entries
