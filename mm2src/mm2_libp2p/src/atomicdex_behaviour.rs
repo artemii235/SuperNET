52d2a0d860e5eca222925db9fc2af6e4054be61d
Artem Pikulin <ortgma@gmail.com>
2020-10-22 20:57:22 +0700

    Increase CHANNEL_BUF_SIZE to 1024.

--- mm2src/mm2_libp2p/src/atomicdex_behaviour.rs
+++ mm2src/mm2_libp2p/src/atomicdex_behaviour.rs
@@ -41,7 +41,7 @@ pub const PEERS_TOPIC: &str = "PEERS";
 const CONNECTED_RELAYS_CHECK_INTERVAL: Duration = Duration::from_secs(30);
 const ANNOUNCE_INTERVAL: Duration = Duration::from_secs(600);
 const ANNOUNCE_INITIAL_DELAY: Duration = Duration::from_secs(60);
-const CHANNEL_BUF_SIZE: usize = 100;
+const CHANNEL_BUF_SIZE: usize = 1024;
 
 impl libp2p::core::Executor for &SwarmRuntime {
     fn exec(&self, future: Pin<Box<dyn Future<Output = ()> + Send>>) { self.0.spawn(future); }
